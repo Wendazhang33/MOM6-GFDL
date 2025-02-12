a1f6276de6e30fdc13bcf597f0d0a4d7229af4c5
Wenda Zhang <zhangwenda33@hotmail.com>
2025-02-12 17:15:39 -0500

    Modification for WENO7TH_ENSTRO: Changed Sadourny Energy to Arakawa_Hsu for the lowest order

--- src/core/MOM_CoriolisAdv.F90
+++ src/core/MOM_CoriolisAdv.F90
@@ -604,7 +604,8 @@ subroutine CorAdCalc(u, v, h, uh, vh, CAu, CAv, OBC, AD, G, GV, US, CS, pbv, Wav
     ! scheme.  All are defined at u grid points.
 
     if ((CS%Coriolis_Scheme == ARAKAWA_HSU90) &
-            .or. (CS%Coriolis_Scheme == wenovi7th_PV_ENSTRO)) then
+            .or. (CS%Coriolis_Scheme == wenovi7th_PV_ENSTRO) &
+            .or. (CS%Coriolis_Scheme == wenovi7th_ENSTRO)) then
       do j=Jsq,Jeq+1
         do I=is-1,Ieq
           a(I,j) = (q(I,J) + (q(I+1,J) + q(I,J-1))) * C1_12
@@ -1126,9 +1127,12 @@ subroutine CorAdCalc(u, v, h, uh, vh, CAu, CAv, OBC, AD, G, GV, US, CS, pbv, Wav
         !        q_u = abs_vort(I,J)
         !    endif
         else
-          CAu(I,j,k) = 0.25 * &
-               ((q(I,J) * (vh(i+1,J,k) + vh(i,J,k))) + &
-                (q(I,J-1) * (vh(i,J-1,k) + vh(i+1,J-1,k)))) * G%IdxCu(I,j) ! Sadourny energy
+!          CAu(I,j,k) = 0.25 * &
+!               ((q(I,J) * (vh(i+1,J,k) + vh(i,J,k))) + &
+!                (q(I,J-1) * (vh(i,J-1,k) + vh(i+1,J-1,k)))) * G%IdxCu(I,j) ! Sadourny energy
+          ! (Global) Energy and (Local) Enstrophy conserving, Arakawa & Hsu 1990
+          CAu(I,j,k) = (((a(I,j) * vh(i+1,J,k)) +  (c(I,j) * vh(i,J-1,k)))  + &
+                      ((b(I,j) * vh(i,J,k)) +  (d(I,j) * vh(i+1,J-1,k)))) * G%IdxCu(I,j)
         endif
 
       enddo ; enddo
@@ -1465,6 +1469,11 @@ subroutine CorAdCalc(u, v, h, uh, vh, CAu, CAv, OBC, AD, G, GV, US, CS, pbv, Wav
 !              q_v = q(I,J)
 !          endif
 !        endif
+!        elseif ((Ih_q(I-1,J) < CS%Ih_thresh) .or. (Ih_q(I,J) < CS%Ih_thresh)) then
+!          CAv(i,J,k) = - (((a(I-1,j)   * uh(I-1,j,k)) + &
+!                         (c(I,j+1)   * uh(I,j+1,k)))  &
+!                      + ((b(I,j)     * uh(I,j,k)) +   &
+!                         (d(I-1,j+1) * uh(I-1,j+1,k)))) * G%IdyCv(i,J)
 
         else
       !    CAv(i,J,k) = - 0.25* &
@@ -1475,6 +1484,7 @@ subroutine CorAdCalc(u, v, h, uh, vh, CAu, CAv, OBC, AD, G, GV, US, CS, pbv, Wav
                          (c(I,j+1)   * uh(I,j+1,k)))  &
                       + ((b(I,j)     * uh(I,j,k)) +   &
                          (d(I-1,j+1) * uh(I-1,j+1,k)))) * G%IdyCv(i,J)
+!          CAv(i,J,k) = 0.0
         endif
 
       enddo ; enddo
@@ -1604,9 +1614,14 @@ subroutine CorAdCalc(u, v, h, uh, vh, CAu, CAv, OBC, AD, G, GV, US, CS, pbv, Wav
         !        q_v = abs_vort(I,J)
         !    endif
         else
-          CAv(i,J,k) = - 0.25* &
-              ((q(I-1,J)*(uh(I-1,j,k) + uh(I-1,j+1,k))) + &
-               (q(I,J)*(uh(I,j,k) + uh(I,j+1,k)))) * G%IdyCv(i,J)  ! Sadourny Energy
+!          CAv(i,J,k) = - 0.25* &
+!              ((q(I-1,J)*(uh(I-1,j,k) + uh(I-1,j+1,k))) + &
+!               (q(I,J)*(uh(I,j,k) + uh(I,j+1,k)))) * G%IdyCv(i,J)  ! Sadourny Energy
+      ! (Global) Energy and (Local) Enstrophy conserving, Arakawa & Hsu 1990
+          CAv(i,J,k) = - (((a(I-1,j)   * uh(I-1,j,k)) + &
+                         (c(I,j+1)   * uh(I,j+1,k)))  &
+                      + ((b(I,j)     * uh(I,j,k)) +   &
+                         (d(I-1,j+1) * uh(I-1,j+1,k)))) * G%IdyCv(i,J)
         endif
 
       enddo ; enddo
